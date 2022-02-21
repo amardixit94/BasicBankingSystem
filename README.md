0116423b6d84824df74fc7cd41ffe02354683400
amardixit94 <86550850+amardixit94@users.noreply.github.com>
2022-02-21 23:09:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BasicBankingSystem
-This project is based on money transaction between two users. The transfer table shows all users along with their balance and the transaction history shows every transaction along with date and time of transaction and the sender and receiver as well.
+***I have created the Basic Banking System as an intern for the Sparks Foundation.***
+#GRIPMAY21 
+
+Intern @ Spark Foundation
+
+Task 1: Basic Banking System
+
+The Basic Banking System website function is to transfer money from one account to another account.
+
+**FrontEnd: HTML, CSS, JS ***
+***Backend: PHP***
+***Database: MySQL***
+      
---
+++ index.php
@@ -0,0 +1,39 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Banking System</title>
+  </head>
+
+  <body style="background-color: black;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('BackgroundImage.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    width:100%;
+    background-position:;
+  }
+  h1{
+    color:white;
+  }
+</style>
+<h1>W</h1><br>
+<h1>E</h1><br>
+<h1>L</h1><br>
+<h1>C</h1><br>
+<h1>O</h1><br>
+<h1>M</h1><br>
+<h1>E</h1><br>
+    </main>
+  </body>
+</html>
